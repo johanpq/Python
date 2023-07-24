2aef724f29f89382336bf41310a668322eaf8cfc
johanpq <johanp.queiroz@gmail.com>
2023-07-24 09:26:01 -0300

    exercising

---
+++ learning_input_output/inputs_and_outputs/exercises/ex1.py
@@ -0,0 +1,18 @@
+print("Welcome!")
+
+Nome = input("What's your name? ")
+
+print("Welcome ", Nome)
+
+Idade = input("How old are you? ")
+
+print("You're", Idade, "How young you are! ")
+
+print("Just by that I can know the year you born ")
+print("Check it out")
+
+N_Idade = int(Idade)
+
+Ano_nascimento = 2023 - N_Idade
+
+print("You born in", Ano_nascimento)
---
+++ learning_input_output/inputs_and_outputs/exercises/ex2.py
@@ -0,0 +1,7 @@
+C = input("Type the temperature in Celsius ")
+
+Celsius = float(C)
+
+F = (Celsius * 1.8) + 32
+
+print("The temperature in Fahrenheit is", F)
