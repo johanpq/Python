9604ed959a65bf93f63b7499d423f3f1ba3c6d24
johanpq <johanp.queiroz@gmail.com>
2023-07-29 16:43:02 -0300

    adding conditions

---
+++ conditions/learning_conditions.py
@@ -0,0 +1,16 @@
+x = 18
+
+if x > 10:
+    print("OK")
+
+#ele não roda igual C (colocando () na condição)
+#além disso, tem que colocar : depois da condição 
+#ele só vai rodar indentado
+
+if x < 10:
+    print("Criança")
+else: 
+    print("Não deixa de ser Criança")
+    
+#elif é o mesmo que else if
+
