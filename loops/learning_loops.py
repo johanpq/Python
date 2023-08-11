3778f115a75ff348252d08ab2c8aa218dade2b65
johanpq <johanp.queiroz@gmail.com>
2023-08-11 15:10:12 -0300

    learned loops

---
+++ loops/learning_loops.py
@@ -0,0 +1,14 @@
+for i in range(5):
+    print("A", i)
+
+#i já e declarado como 0, e vai até 4, totalizando 5 loops
+
+print("==============================")
+
+contador = 0
+
+while contador <= 5:
+    print("A", contador)
+    contador+= 1
+    
+#ele não aceita ++ como incremento, e sim +=
